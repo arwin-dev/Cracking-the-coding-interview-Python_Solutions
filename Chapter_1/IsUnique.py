e24e9cd711d0dd11e421f8d5f3b7320addddf7c2
arwin-dev <arwin.dev@outlook.com>
2022-09-30 16:54:00 -0400

    IsUnique Solution

--- Chapter_1/IsUnique.py
+++ Chapter_1/IsUnique.py
@@ -1,30 +1,14 @@
-class HashTable:
-    def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
-        
-    def get_hash(self,key):
-        h=0
-        for char in key:
-            h+= ord(char)
-        return h % self.MAX
+def IsUnique(string):
+    if len(string) > 128:
+        return False
     
-    def __setitem__(self,key,val):
-        h = self.get_hash(key)
-        self.arr[h] = val
-    
-    def __getitem__(self,key):
-        h =self.get_hash(key)
-        return self.arr[h]
-    
-    def __delitem__(self,key):
-        h = self.get_hash(key)
-        self.arr[h] = None
+    HashMap = {}
+    for char in string:
+        val = ord(char)
+        if val in HashMap:
+            return False
+        HashMap[val] = char
+        print(HashMap)
+    return True
 
-    
-t = HashTable()
-t['march 6'] = 130
-t['march 9'] = 125
-t['march 1'] = 100
-del t['march 6']
-print(t.arr)
+print(IsUnique('abcdd'))
